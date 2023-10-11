4f9cf86a68d4d1075f41bbcec671004a350fa22d
Dhruv Patel <99269dhruvpatel@gmail.com>
2023-10-11 14:59:30 +0530

    modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       )}>
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   )
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import MaxWidthWrapper from "./MaxWidthWrapper"
+
+const Footer = () => {
+    return (
+        <footer className="flex h-14 inset-x-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWidthWrapper>
+                <div className="flex flex-col">
+                    <div className="font-semibold text-center redshade mt-5 ">Designed by: Dhaval Bhimani and Dhruv Patel</div>
+                </div>
+            </MaxWidthWrapper>
+        </footer>
+    )
+}
+
+export default Footer

57b838438f48724dbeb1cd730182a049a763f389
Dhaval Bhimani <dhaval.bhimani.btech2022@sitpune.edu.in>
2024-01-19 11:34:06 +0530

    removed kinde

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
-import {LoginLink, RegisterLink} from "@kinde-oss/kinde-auth-nextjs/server"
 import { ArrowRight } from "lucide-react";
 
 const Navbar = () => {
@@ -19,27 +18,6 @@ const Navbar = () => {
 
                     <div className="hidden items-center space-x-4 sm:flex">
                         <>
-                            {/* <Link 
-                                href='/pricing' 
-                                className={buttonVariants({
-                                    variant: "ghost",
-                                    size: "sm",
-                                })}>
-                                Pricing
-                            </Link> */}
-                            <LoginLink
-                                className={buttonVariants({
-                                    variant: "ghost",
-                                    size: "sm",
-                                })}>
-                                    Sign in
-                            </LoginLink>
-                            <RegisterLink
-                                className={buttonVariants({
-                                    size: "sm",
-                                })}>
-                                    Get started <ArrowRight className="ml-1.5 h-5 w-5 "/>
-                            </RegisterLink>
                         </>
                     </div>
                 </div>

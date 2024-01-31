a286cf0118af6bc8c664e5c379fbe520171e4c22
Dhaval Bhimani <dhaval.bhimani.btech2022@sitpune.edu.in>
2024-01-31 15:28:13 +0530

    fixed feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,8 +101,8 @@ export default function Home() {
             <li className="md:flex-1">
               <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
                 <span className="text-sm font-medium text-violet-600">Step 1</span>
-                <span className="text-xl font-semibold">Sign up for an account</span>
-                <span className="mt-2 text-zinc-700">Sign up using your PRN</span>
+                <span className="text-xl font-semibold">Login your account</span>
+                <span className="mt-2 text-zinc-700">Login your account using your PRN and password(check your email!).</span>
               </div>
             </li>
             <li className="md:flex-1">

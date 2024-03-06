7b5f87cb2722cd97a504a46fba02194865d9b912
dhruvpatel99269 <99269dhruvpatel@gmail.com>
2024-03-06 11:46:20 +0530

    signin backend

---
+++ src/app/api/user/sign-in/route.ts
@@ -0,0 +1,41 @@
+import { FormSchema } from "@/app/validationSchema";
+import { db } from "@/lib/db";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+        const JWT_SECRET = 'your_hardcoded_secret_key';
+
+        const validation = FormSchema.safeParse(body);
+        if (!validation.success) {
+            return NextResponse.json(validation.error.errors, { status: 400 });
+        }
+
+        const email = await db.user.findUnique({
+            where: {
+                email: body.email
+            }
+        })
+        if(!email){
+            return NextResponse.json({error: "Email does not exist"}, {status: 400})
+        }
+        console.log("Email exists")
+
+        const passwordMatch = await compare(body.password, email.password);
+        if (!passwordMatch) {
+            console.log("Incorrect password!");
+            return NextResponse.json({ message: "Invalid credentials" }, { status: 401 });
+        }
+
+        const token = sign({ emailId: email.id }, JWT_SECRET, {
+            expiresIn: "30min",
+        });
+  
+        return NextResponse.json({ token, email }, { status: 200 });
+    }catch(error:any){
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}

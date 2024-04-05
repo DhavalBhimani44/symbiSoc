4020c848846d316e48bbf09d8b0c018f7872e1eb
dhruvpatel99269 <99269dhruvpatel@gmail.com>
2024-04-05 10:25:08 +0530

    added sidebar

--- src/app/incharge/registeredEvents/page.tsx
+++ src/app/incharge/registeredEvents/page.tsx
@@ -4,11 +4,11 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import RegisteredBasicCard from "@/components/RegisteredBasicCard";
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function InchargePage() {
     const router = useRouter();
-    const [ events,setEvents ] = useState([]);
+    const [events, setEvents] = useState([]);
 
     useEffect(() => {
         const registeredEvents = async () => {
@@ -24,34 +24,32 @@ export default function InchargePage() {
 
     return (
         <>
-            <div className="flex flex-col justify-center itmes-center bg-white">
-                <div className='w-full h-full m-auto'>
-                    <div className="flex justify-around w-full z-10 top-14 sticky">
-                        <div className="flex w-1/3 bg-red-100">
-                            <Link href="/incharge/createEvent" className="w-full hover:border-b-4 hover:border-red-400 hover:p-2 flex justify-around text-sm sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl p-1 lg:p-2 xl:p-2">
-                                Create Event
-                            </Link>
-                        </div>
-                        <div className="flex w-1/3 bg-red-100">
-                            <Link href="/incharge/viewEvent" className="w-full hover:border-b-4 hover:border-red-400 hover:p-2 flex justify-around text-sm sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl p-1 lg:p-2 xl:p-2">
-                                View Events
-                            </Link>
-                        </div>
-                        <div className="flex w-1/3 bg-blue-100">
-                            <Link href="/incharge/registeredEvents" className="w-full border-b-4 border-red-600 hover:p-2 flex justify-around text-sm sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl p-1 lg:p-2 xl:p-2">
-                                Registered Events
-                            </Link>
-                        </div>
+            <div className="flex bg-white">
+                <div className="flex flex-col w-1/4 h-screen z-10 top-14 sticky text-slate-300 bg-neutral-900">
+                <div className="flex w-full">
+                        <Link href="/incharge/createEvent" className="w-full hover:border-r-4 hover:border-red-400 hover:p-2 flex justify-around text-sm sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl p-1 lg:p-2 xl:p-2">
+                            Create Event
+                        </Link>
                     </div>
+                    <div className="flex w-full">
+                        <Link href="/incharge/viewEvent" className="w-full hover:border-r-4 hover:border-red-400 hover:p-2 flex justify-around text-sm sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl p-1 lg:p-2 xl:p-2">
+                            View Events
+                        </Link>
+                    </div>
+                    <div className="flex w-full">
+                        <Link href="/incharge/registeredEvents" className="w-full border-r-4 border-red-600 hover:p-2 flex justify-around text-sm sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl p-1 lg:p-2 xl:p-2">
+                            Registered Events
+                        </Link>
+                    </div>
+                </div>
 
-                    <div className='w-full h-full bg-fixed lg:w-full xl:w-full px-4 sm:px-2 md:px-4 lg:px-4 xl:px-4 py-2 shadow-2xl text-gray-200 relative antialiased' style={{ backgroundImage: 'url("/bg4.jpg")', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundAttachment: 'fixed', height: '100', width: '100' }}>
-                        <div className="flex flex-col w-full">
-                            <div className="flex text-6xl w-full justify-center items-center text-white">
-                                <h1>Registered Events</h1>
-                            </div>
-                            <div className="flex text-4xl w-full">
-                                <RegisteredBasicCard />
-                            </div>
+                <div className='w-3/4 h-screen bg-fixed px-4 sm:px-2 md:px-4 lg:px-4 xl:px-4 py-2 shadow-2xl text-gray-200 relative antialiased' style={{ backgroundImage: 'url("/bg4.jpg")', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundAttachment: 'fixed', height: '100', width: '100' }}>
+                    <div className="flex flex-col w-full">
+                        <div className="flex text-6xl w-full justify-center items-center text-white">
+                            <h1>Registered Events</h1>
+                        </div>
+                        <div className="flex text-4xl w-full">
+                            <RegisteredBasicCard />
                         </div>
                     </div>
                 </div>

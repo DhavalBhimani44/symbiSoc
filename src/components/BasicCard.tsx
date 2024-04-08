9d2ee842f5c03602593e8594c59c1955ee51a8e6
dhruvpatel99269 <99269dhruvpatel@gmail.com>
2024-04-08 19:31:34 +0530

    card animation

--- src/components/BasicCard.tsx
+++ src/components/BasicCard.tsx
@@ -8,9 +8,8 @@ import Typography from '@mui/joy/Typography';
 import Image from 'next/image';
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { date } from 'zod';
 import { useRouter } from 'next/navigation';
-import { toast, useToast } from '@/components/ui/use-toast';
+import { toast } from '@/components/ui/use-toast';
 
 interface BasicCardProps {
   userRole: "student" | "incharge" | "admin" | "faculty";
@@ -42,39 +41,37 @@ export default function BasicCard({ userRole }: BasicCardProps) {
       toast({
         duration: 2000,
         description: 'Event deleted successfully'
-      })
+      });
       setEvents(events.filter(event => event.eventId !== eventId));
     } catch (error) {
       console.error('Error deleting event:', error);
       toast({
         duration: 2000,
         description: 'Error deleting event'
-      })
+      });
     }
   };
 
   const handleRegister = async (eventId: number) => {
     try {
-      // Perform registration
       await axios.post('/api/event/registerEvent', {
         eventId: eventId
       });
-      // Redirect to the registered events page
       if (userRole === 'incharge') {
-        router.push('/incharge/registeredEvents')
+        router.push('/incharge/registeredEvents');
       } else {
-        router.push('/student/registeredEvents')
+        router.push('/student/registeredEvents');
       }
       toast({
         duration: 2000,
-        description: 'Registration successfull'
-      })
+        description: 'Registration successful'
+      });
     } catch (error) {
       console.error('Error registering for event:', error);
       toast({
         duration: 2000,
         description: 'Registration failed'
-      })
+      });
     }
   };
 
@@ -90,23 +87,23 @@ export default function BasicCard({ userRole }: BasicCardProps) {
     } else {
       router.push('/student');
     }
-  }
+  };
 
   return (
     <div className='w-full my-6'>
       <ul>
         <div className='flex flex-wrap justify-around w-full'>
-          {events.map((event: any) =>
-            <Card key={event} sx={{ width: 320 }} className="mx-2 my-4">
+          {events.map((event: any) => (
+            <Card
+              key={event}
+              className="mx-2 my-4 shadow-cyan-600 shadow-md transition-transform transform hover:translate-y-[-5px] hover:scale-105 hover:shadow-md hover:shadow-blue-700"
+            >
               <div>
                 <AspectRatio minHeight="120px" maxHeight="200px">
                   <Image width={150} height={150} src="/cbc-logo.png" alt='image' loading='lazy' />
                 </AspectRatio>
 
                 <div>
-                  {/* <Typography level="body-sm">
-                    {event.eventDate}
-                  </Typography> */}
                   <Typography level="title-lg">
                     {event.eventName}
                   </Typography>
@@ -118,13 +115,13 @@ export default function BasicCard({ userRole }: BasicCardProps) {
                       Event Description: {event.eventDescription}
                     </div>
                     <div>
-                      Organnising Club: {event.organisingClub}
+                      Organising Club: {event.organisingClub}
                     </div>
                   </li>
                 </div>
               </div>
 
-              <CardContent orientation="horizontal">
+              <CardContent orientation="horizontal" className="flex justify-between items-center">
                 {userRole === "incharge" && (
                   <Button
                     variant="solid"
@@ -135,8 +132,7 @@ export default function BasicCard({ userRole }: BasicCardProps) {
                       handleDelete(event.eventId)
                       window.location.reload()
                     }}
-                    sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
-
+                    sx={{ fontWeight: 600 }}
                   >
                     Delete
                   </Button>
@@ -146,9 +142,8 @@ export default function BasicCard({ userRole }: BasicCardProps) {
                   size="md"
                   color="primary"
                   aria-label="Register Event"
-                  sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
                   onClick={() => handleRegister(event.eventId)}
-
+                  sx={{ fontWeight: 600 }}
                 >
                   Register
                 </Button>
@@ -156,15 +151,15 @@ export default function BasicCard({ userRole }: BasicCardProps) {
                   variant="solid"
                   size="md"
                   color="primary"
-                  aria-label="Register Event"
-                  sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
+                  aria-label="More Info"
                   onClick={() => eventDetail(event.eventId)}
+                  sx={{ fontWeight: 600 }}
                 >
                   More Info
                 </Button>
               </CardContent>
-            </Card >
-          )}
+            </Card>
+          ))}
         </div>
       </ul>
     </div>
